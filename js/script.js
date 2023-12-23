e2153df4dbd49f6ca4dd6403ae91ffeeb44eba7e
DdimaPos <postoroncadumitru@gmail.com>
2023-12-23 13:07:16 +0200

    try to display a table

---
+++ js/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch the CSV file
+    fetch('../data/Profesori.csv')
+        .then(response => response.text())
+        .then(data => {
+            // Parse CSV data
+            const rows = data.split('\n');
+            const headers = rows[0].split(',');
+
+            // Create HTML table headers
+            const table = document.getElementById('excelTable');
+            const thead = document.createElement('thead');
+            const headerRow = document.createElement('tr');
+
+            headers.forEach(header => {
+                const th = document.createElement('th');
+                th.textContent = header;
+                headerRow.appendChild(th);
+            });
+
+            thead.appendChild(headerRow);
+            table.appendChild(thead);
+
+            // Create HTML table body
+            const tbody = document.createElement('tbody');
+            rows.slice(1).forEach(row => {
+                const rowData = row.split(',');
+                const tr = document.createElement('tr');
+
+                rowData.forEach(cellData => {
+                    const td = document.createElement('td');
+                    td.textContent = cellData;
+                    tr.appendChild(td);
+                });
+
+                tbody.appendChild(tr);
+            });
+
+            table.appendChild(tbody);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
